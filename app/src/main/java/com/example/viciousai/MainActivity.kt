6d98f7757f5b8e87977e29c9aadfcc3aa2911768
mdaviaud <matthis.daviaud@etudiant.univ-rennes1.fr>
2026-02-16 17:29:34 -0500

    Ajout d'un début de page d'aide

--- app/src/main/java/com/example/viciousai/MainActivity.kt
+++ app/src/main/java/com/example/viciousai/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.viciousai
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -31,6 +32,10 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 startAnalysis()
             }
+
+        findViewById<ImageButton>(R.id.helpButton).setOnClickListener {
+            Toast.makeText(this, "Page d'aide à venir", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onRequestPermissionsResult(
